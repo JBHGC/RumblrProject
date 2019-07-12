613bc4977b2f0a5aa8f962530b0b067b235f56ab
stadmin <stadmin@nyc.yearup.org>
2019-07-12 13:07:19 -0400

    fxing if statement

--- server.rb
+++ server.rb
@@ -121,15 +121,15 @@ end
 
 post '/createpost' do
   for each in params
-    each = ''
-    redirect '/createpost'
-  else
-    post_title = params[:title]
-    post_content = params[:content]
-    post_user = User.find(session[:user_id]).user_name
-    post_tags = params[:tags]
-    @usrPost = Post.new(title: post_title, content: post_content, user_name: post_user, tags: post_tags)
-    @usrPost.save
-    redirect '/'
+    if each = ''
+      redirect '/createpost'
+    else
+      post_title = params[:title]
+      post_content = params[:content]
+      post_user = User.find(session[:user_id]).user_name
+      post_tags = params[:tags]
+      @usrPost = Post.new(title: post_title, content: post_content, user_name: post_user, tags: post_tags)
+      @usrPost.save
+      redirect '/'
   end
 end

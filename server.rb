bf04dc0e064f9d6c80c4f13a8ccaa992af74c74e
stadmin <stadmin@nyc.yearup.org>
2019-07-12 13:11:09 -0400

    adding an end statement

--- server.rb
+++ server.rb
@@ -123,13 +123,13 @@ post '/createpost' do
   for each in params
     if each = ''
       redirect '/createpost'
-    else
-      post_title = params[:title]
-      post_content = params[:content]
-      post_user = User.find(session[:user_id]).user_name
-      post_tags = params[:tags]
-      @usrPost = Post.new(title: post_title, content: post_content, user_name: post_user, tags: post_tags)
-      @usrPost.save
-      redirect '/'
+    end
   end
+  post_title = params[:title]
+  post_content = params[:content]
+  post_user = User.find(session[:user_id]).user_name
+  post_tags = params[:tags]
+  @usrPost = Post.new(title: post_title, content: post_content, user_name: post_user, tags: post_tags)
+  @usrPost.save
+  redirect '/'
 end
